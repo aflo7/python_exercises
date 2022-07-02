821c25c215195d182e904ed21b4ce526cbdf0c22
Andres Flores <andresflo883@gmail.com>
2022-07-02 16:24:28 -0400

    .

--- bank/app.py
+++ bank/app.py
@@ -1,9 +1,40 @@
 import os
+from cryptography.fernet import Fernet
+
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import credentials, db
 from dotenv import load_dotenv
+from regex import E
 load_dotenv()
 
-token = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
-cred = credentials.Certificate(token)
-firebase_admin.initialize_app(cred)
+googleToken = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+cryptoToken = os.environ.get("CRYPTOCODE")
+cred = credentials.Certificate(googleToken)
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://bank-79eda-default-rtdb.firebaseio.com/'
+})
+bank_users = db.reference('/users')
+
+def get_number_of_users():
+    current_users = bank_users.get()
+    if current_users is None:
+        return 0
+    return len(current_users)
+
+def create_new_user(name=None, amount="0", membership_status=None):
+    size = get_number_of_users()
+    
+    bank_users.update({size: {
+        'name': name,
+        'amount': amount,
+        'membership_status': membership_status
+    }})
+    
+
+# def encrypt(message: bytes, key: bytes) -> bytes:
+#     return Fernet(key).encrypt(message)
+
+# def decrypt(token: bytes, key: bytes) -> bytes:
+#     return Fernet(key).decrypt(token)
+
+# create_new_user("Andres", "150", "silver")
--- bank/tempCodeRunnerFile.py
+++ bank/tempCodeRunnerFile.py
@@ -1,5 +1,1 @@
-token = os.environ.get("firebase-service-key")
 
-cred = credentials.Certificate(f"path/to/{token}.json")
-firebase_admin.initialize_app(cred)
-ref = db.reference("/")
